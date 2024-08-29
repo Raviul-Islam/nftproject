bce20df4b7e501ce26d0c2de8c9c089a7b76d4b6
Raviul-Islam <raviulislam3@gmail.com>
2024-08-29 15:45:01 +0530

    Discover & Sidebar

---
+++ .history/components/NavBar/Sidebar/Sidebar_20240829150637.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import GrClose from "react-icons/gr";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+  TiArrowSortedDown,
+  TiArrowSortedDown,
+} from "react-icons/ti";
+import Style from "./Sidebar.module.css";
+
+const Sidebar = () => {
+  return <div>SideBar</div>;
+};
+
+export default Sidebar;
